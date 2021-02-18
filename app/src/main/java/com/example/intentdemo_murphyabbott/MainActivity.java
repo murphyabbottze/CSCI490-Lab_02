c7eb917e5d7c38ab5d1bbd0ba1b24ab4ca80b0d8
Zeke Murphy <murphyabbottze@g.cofc.edu>
2021-02-18 18:30:48 -0500

    final product

--- app/src/main/java/com/example/intentdemo_murphyabbott/MainActivity.java
+++ app/src/main/java/com/example/intentdemo_murphyabbott/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.intentdemo_murphyabbott;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Intent i;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+
+        button = findViewById(R.id.button);
+        i = new Intent(this, SecondActivity.class);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+               Toast.makeText(getApplicationContext(),"I DID IT!",Toast.LENGTH_SHORT).show();
+                String s = editText.getText().toString();
+                i.putExtra("testString", s);
+
+                startActivity(i);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/intentdemo_murphyabbott/SecondActivity.java
@@ -0,0 +1,26 @@
+package com.example.intentdemo_murphyabbott;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent i = getIntent();
+        String s = i.getExtras().getString("testString");
+
+        textView2 = findViewById(R.id.textView2);
+
+        textView2.setText(s);
+    }
+}
